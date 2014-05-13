74ff3916d42271b26cee384ef1dbc63f63356328
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-12 18:45:54 -0700

    Solution with for loop

--- src/OutlineParser.js
+++ src/OutlineParser.js
@@ -1,2 +1,12 @@
 module.exports = function(string) {
+  if (string === '') return [];
+  var out = [];
+  string.trim().split(/\n+/).forEach(function(item) {
+    var target = out;
+    for (var i = 0; item[i] === ' '; i++) {
+      target = target[target.length-1].children;
+    }
+    target.push({name: item.trim(), children: []});
+  });
+  return out;
 };
