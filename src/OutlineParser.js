cec94aa1d8cae240437c5f4e4ae9641f6b48d25d
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-13 19:03:50 -0700

    Solution at lightning talks

--- src/OutlineParser.js
+++ src/OutlineParser.js
@@ -1,2 +1,13 @@
 module.exports = function(string) {
+  if (string === '') return [];
+  var out = [];
+  string.trim().split(/\n+/).forEach(function(item) {
+    var newItem = {name: item.trim(), children: []};
+    var target = out;
+    for (var i = 0; item[i] === ' '; i++) {
+      target = target[target.length-1].children;              
+    }
+    target.push(newItem);      
+  });
+  return out;
 };
