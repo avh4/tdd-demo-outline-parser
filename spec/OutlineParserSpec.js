d4b1b44b169f3a418136301e6d95a2ac79219475
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-27 18:13:42 -0700

    Solution for Youtube

--- spec/OutlineParserSpec.js
+++ spec/OutlineParserSpec.js
@@ -2,99 +2,112 @@ var expect = require('chai').expect;
 var OutlineParser = require('../src/OutlineParser');
 
 describe('OutlineParser', function() {
-  // it('handles a single item', function() {
-  //   expect(OutlineParser('Homework'))
-  //   .to.eql([
-  //     {name:'Homework', children:[]}
-  //   ]);
-  // });
-  //
-  // it('handles an empty input', function() {
-  //   expect(OutlineParser(''))
-  //   .to.eql([]);
-  // });
-  //
-  // it('handles a simple list', function() {
-  //   expect(OutlineParser('A\nB\nC'))
-  //   .to.eql([
-  //     {name:'A', children:[]},
-  //     {name:'B', children:[]},
-  //     {name:'C', children:[]}
-  //   ]);
-  // });
-  //
-  // it('ignores blank lines', function() {
-  //   expect(OutlineParser('A\n\nB\nC'))
-  //   .to.eql([
-  //     {name:'A', children:[]},
-  //     {name:'B', children:[]},
-  //     {name:'C', children:[]}
-  //   ]);
-  // });
-  //
-  // it('trims blank lines', function() {
-  //   expect(OutlineParser('\n\nA\nB\nC\n\n'))
-  //   .to.eql([
-  //     {name:'A', children:[]},
-  //     {name:'B', children:[]},
-  //     {name:'C', children:[]}
-  //   ]);
-  // });
-  //
-  // it('handles one child element', function() {
-  //   expect(OutlineParser('A\n a1'))
-  //   .to.eql([
-  //     {name: 'A', children: [
-  //       {name:'a1', children:[]}
-  //     ]}
-  //   ]);
-  // });
-  //
-  // it('handles multiple children', function() {
-  //   expect(OutlineParser('A\n a1\n a2\n a3'))
-  //   .to.eql([
-  //     {name: 'A', children: [
-  //       {name:'a1', children:[]},
-  //       {name:'a2', children:[]},
-  //       {name:'a3', children:[]}
-  //     ]}
-  //   ]);
-  // });
-  //
-  // it('handles multiple parents', function() {
-  //   expect(OutlineParser('A\n a1\n a2\nB\nC\n c1'))
-  //   .to.eql([
-  //     {name: 'A', children: [
-  //       {name:'a1', children:[]},
-  //       {name:'a2', children:[]}
-  //     ]},
-  //     {name:'B', children:[]},
-  //     {name: 'C', children: [
-  //       {name:'c1', children:[]}
-  //     ]}
-  //   ]);
-  // });
-  //
-  // it('handles grandchildren', function() {
-  //   expect(OutlineParser('A\n a1\n  a1i'))
-  //   .to.eql([
-  //     {name: 'A', children: [
-  //       {name: 'a1', children: [
-  //         {name:'a1i', children:[]}
-  //       ]}
-  //     ]}
-  //   ]);
-  // });
-  //
-  // it('handles grandchildren for parents besides the first', function() {
-  //   expect(OutlineParser('A\nB\n b1\n  b1i'))
-  //   .to.eql([
-  //     {name: 'A', children: []},
-  //     {name: 'B', children: [
-  //       {name: 'b1', children: [
-  //         {name: 'b1i', children: []}
-  //       ]}
-  //     ]}
-  //   ]);
-  // });
+  it('handles a single item', function() {
+    expect(OutlineParser('Homework'))
+    .to.eql([
+      {name:'Homework', children:[]}
+    ]);
+  });
+
+  it('handles an empty input', function() {
+    expect(OutlineParser(''))
+    .to.eql([]);
+  });
+
+  it('handles a simple list', function() {
+    expect(OutlineParser('A\nB\nC'))
+    .to.eql([
+      {name:'A', children:[]},
+      {name:'B', children:[]},
+      {name:'C', children:[]}
+    ]);
+  });
+
+  it('ignores blank lines', function() {
+    expect(OutlineParser('A\n\nB\nC'))
+    .to.eql([
+      {name:'A', children:[]},
+      {name:'B', children:[]},
+      {name:'C', children:[]}
+    ]);
+  });
+
+  it('trims blank lines', function() {
+    expect(OutlineParser('\n\nA\nB\nC\n\n'))
+    .to.eql([
+      {name:'A', children:[]},
+      {name:'B', children:[]},
+      {name:'C', children:[]}
+    ]);
+  });
+
+  it('handles one child element', function() {
+    expect(OutlineParser('A\n a1'))
+    .to.eql([
+      {name: 'A', children: [
+        {name:'a1', children:[]}
+      ]}
+    ]);
+  });
+
+  it('handles multiple children', function() {
+    expect(OutlineParser('A\n a1\n a2\n a3'))
+    .to.eql([
+      {name: 'A', children: [
+        {name:'a1', children:[]},
+        {name:'a2', children:[]},
+        {name:'a3', children:[]}
+      ]}
+    ]);
+  });
+
+  it('handles multiple parents', function() {
+    expect(OutlineParser('A\n a1\n a2\nB\nC\n c1'))
+    .to.eql([
+      {name: 'A', children: [
+        {name:'a1', children:[]},
+        {name:'a2', children:[]}
+      ]},
+      {name:'B', children:[]},
+      {name: 'C', children: [
+        {name:'c1', children:[]}
+      ]}
+    ]);
+  });
+
+  it('handles grandchildren', function() {
+    expect(OutlineParser('A\n a1\n  a1i'))
+    .to.eql([
+      {name: 'A', children: [
+        {name: 'a1', children: [
+          {name:'a1i', children:[]}
+        ]}
+      ]}
+    ]);
+  });
+
+  it('handles grandchildren for parents besides the first', function() {
+    expect(OutlineParser('A\nB\n b1\n  b1i'))
+    .to.eql([
+      {name: 'A', children: []},
+      {name: 'B', children: [
+        {name: 'b1', children: [
+          {name: 'b1i', children: []}
+        ]}
+      ]}
+    ]);
+  });
+
+  it('handled great-grandchildren', function() {
+    expect(OutlineParser('A\n a1\n  a1i\n   a1iA'))
+    .to.eql([
+      {name: 'A', children: [
+        {name: 'a1', children: [
+          {name: 'a1i', children: [
+            {name: 'a1iA', children: []}
+          ]}
+        ]}
+      ]}
+    ]);
+  });
 });
--- src/OutlineParser.js
+++ src/OutlineParser.js
@@ -1,2 +1,13 @@
 module.exports = function(string) {
+  if (string == '') return [];
+  var result = [];
+  string.trim().split(/\n+/).forEach(function(line) {
+    var newItem = { name: line.trim(), children: [] };
+    var target = result;
+    for (var i = 0; line[i] == ' '; i++) {
+      target = target[target.length - 1].children;
+    }
+    target.push(newItem);
+  });
+  return result;
 };
